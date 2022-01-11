6e43cf652e08dd8682d58c2c06007b1b7ecf6932
luismaster77 <luiseduardoingsis@gmail.com>
2022-01-11 18:47:55 -0500

    Commit 002
    Eliminar lineas inecesarias y dar formato a la clase

--- src/main/java/com/aprende/rest/webservice/restfulwebservices/service/UserDAOService.java
+++ src/main/java/com/aprende/rest/webservice/restfulwebservices/service/UserDAOService.java
@@ -9,61 +9,47 @@ import com.aprende.rest.webservice.restfulwebservices.model.User;
 
 @Component
 public class UserDAOService {
-	
+
 	private static int idCount = 3;
-	
-	private static List<User>  listUser = new ArrayList<>();
+
+	private static List<User> listUser = new ArrayList<>();
 
 	static {
-		listUser.add(new User(1,"Luis","Eduardo",new Date()));
-		listUser.add(new User(2,"Luis","Manuel",new Date()));
-		listUser.add(new User(3,"Abigail","Romo",new Date()));
+		listUser.add(new User(1, "Luis", "Eduardo", new Date()));
+		listUser.add(new User(2, "Luis", "Manuel", new Date()));
+		listUser.add(new User(3, "Abigail", "Romo", new Date()));
 	}
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return listUser;
 	}
-	
+
 	public User save(User user) {
-		if(user.getId()==null) {
+		if (user.getId() == null) {
 			user.setId(++idCount);
 		}
 		listUser.add(user);
 		return user;
 	}
-	
+
 	public User findOne(int idUser) {
-		long startTime = System.currentTimeMillis();
-		Optional<User> result = listUser
-			      .stream().parallel()
-			      .filter(num -> num.getId() == idUser).findAny();
-		
-		long endTime = System.currentTimeMillis();	
-		long tiempofinal = endTime-startTime;
+		Optional<User> result = listUser.stream().parallel().filter(num -> num.getId() == idUser).findAny();
 
-		if(result.isPresent()) {
-			System.out.print("Tiempo ejecución: "+tiempofinal);
+		if (result.isPresent()) {
 			return result.get();
-		}else {
-			System.out.print("Tiempo ejecución: "+tiempofinal);
+		} else {
 			return null;
-
 		}
-		
 	}
-	
+
 	public User findOneVersion(int idUser) {
-		long startTime = System.currentTimeMillis();
 		User user = null;
 		for (User userfind : listUser) {
-			if(userfind.getId() == idUser) {
-				 user = userfind;
+			if (userfind.getId() == idUser) {
+				user = userfind;
 			}
 		}
-		long endTime = System.currentTimeMillis();	
-		long tiempofinal = endTime-startTime;
-		System.out.print("Tiempo ejecución: "+tiempofinal);
 		return user;
-		
+
 	}
 }
